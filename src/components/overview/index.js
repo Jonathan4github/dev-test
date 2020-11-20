17713690a94cc736c06235a3793319055237971a
Jonathan Williams <jonathanwilliams505@gmail.com>
2020-11-20 21:01:44 +0100

    improve overview card

--- src/components/overview/index.js
+++ src/components/overview/index.js
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
+import Progressbar from '../progressbar';
 import Container from './style';
+import Colors from '../../utils/colors';
 
-const Overview = ({imageBackground, color, title}) => {
+const Overview = (props) => {
+    const {imageBackground, color, title, width} = props;
     return (
         <Container imageBackground={imageBackground} color={color}>
-                <div> 
+                <div className="row"> 
                      <h2 className="title">{title}</h2>
+                     <span>Total <p>35</p></span>
                 </div>
-                <div>
-                <div> <br /></div>
-                    <div><br /></div>
+                <div className="row">
+                    <span>Achieved Targets <p>15</p></span>
+                     <span>Targets Left <p>10</p></span>
                 </div>
+                <Progressbar color={Colors.success} width={width} />
+                <div className="row">
+                    <span>{`${width}% completed`}</span>
+                     <span>{`${100 - width}% left`}</span>
+                </div>  
         </Container>
     )
 }
